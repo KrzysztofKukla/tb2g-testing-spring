e3ffa035d480f6363cfc02abbd3f46c0d10c2d2c
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-01-06 14:11:23 +0100

    153 Test Properties  from properties file

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterPropertiesIT.java
@@ -0,0 +1,35 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+/**
+ * @author Krzysztof Kukla
+ */
+
+@ActiveProfiles("externalized")
+@SpringJUnitConfig(classes = HearingInterpreterPropertiesIT.TestConfig.class)
+@TestPropertySource(value = "classpath:yanny.properties")
+class HearingInterpreterPropertiesIT {
+
+    @Autowired
+    private HearingInterpreter hearingInterpreter;
+
+    @Configuration
+    @ComponentScan("org.springframework.samples.petclinic.sfg")
+    static class TestConfig {
+
+    }
+
+    @Test
+    void whatDoYouHear() {
+        Assertions.assertEquals("dupa", hearingInterpreter.whatDoYouHear());
+    }
+
+}

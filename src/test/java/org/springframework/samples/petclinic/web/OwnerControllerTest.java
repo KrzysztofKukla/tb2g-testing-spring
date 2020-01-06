83aae4c2f8053e4631f33e26acf2aad78e34b31c
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-01-06 23:46:19 +0100

    163 mockMvc.perform with param

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -4,6 +4,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -13,8 +14,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.BindingResult;
 
+import java.util.Collections;
 import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.anyString;
 
 /**
  * @author Krzysztof Kukla
@@ -31,6 +36,9 @@ class OwnerControllerTest {
     @Mock
     private Map<String, Object> model;
 
+    @Mock
+    private BindingResult bindingResult;
+
     private MockMvc mockMvc;
 
     @BeforeEach
@@ -45,6 +53,19 @@ class OwnerControllerTest {
             .andExpect(MockMvcResultMatchers.status().isOk())
             .andExpect(MockMvcResultMatchers.view().name("owners/createOrUpdateOwnerForm"))
             .andExpect(MockMvcResultMatchers.model().attribute("owner", Matchers.any(Owner.class)));
+    }
+
+    @Test
+    void processFindFormResultEmpty() throws Exception {
+        //actually this method here is not required, because by default 'clinicService' will return empty collection
+        BDDMockito.when(clinicService.findOwnerByLastName(anyString())).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners")
+            .param("lastName", "dupa") //.params binds 'lastName' field from Owner object
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.view().name("owners/findOwners"));
+//        BDDMockito.then(bindingResult).should().rejectValue(anyString(), anyString(), anyString());
     }
 
 }

1ed78959b7105180c0f23e2efb1a470526e7b5d3
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-01-07 21:54:53 +0100

    165 @Captor & processFindForm test Coverage

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerIT.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerIT.java
@@ -1,6 +1,10 @@
 package org.springframework.samples.petclinic.web;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
@@ -8,6 +12,7 @@ import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 /**
  * @author Krzysztof Kukla
  */
+@ExtendWith(MockitoExtension.class)
 @SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
 class OwnerControllerIT {
 
@@ -16,6 +21,11 @@ class OwnerControllerIT {
 
     @Autowired
     private ClinicService clinicService;
+
+    @AfterEach
+    void tearDown() {
+        BDDMockito.reset(clinicService);
+    }
 
     @Test
     void tempTest() throws Exception {
--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -1,10 +1,14 @@
 package org.springframework.samples.petclinic.web;
 
+import org.assertj.core.util.Lists;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.BDDMockito;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -16,6 +20,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.validation.BindingResult;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 
@@ -41,22 +46,29 @@ class OwnerControllerTest {
 
     private MockMvc mockMvc;
 
+    //Captor requires MockitoExtension
+    @Captor
+    private ArgumentCaptor<String> lastNameValue;
+
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
 
     }
 
+    //Captor for lastName used here
     @Test
-    void initCreationFormTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/owners/new"))
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.view().name("owners/createOrUpdateOwnerForm"))
-            .andExpect(MockMvcResultMatchers.model().attribute("owner", Matchers.any(Owner.class)));
+    void processFindFormNullLastName() throws Exception {
+        //here we did not pass any Owner parameter, so in this case 'lastName' will be null
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners"));
+
+        //here we capture value
+        BDDMockito.then(clinicService).should().findOwnerByLastName(lastNameValue.capture());
+        Assertions.assertEquals("", lastNameValue.getValue());
     }
 
     @Test
-    void processFindFormResultEmpty() throws Exception {
+    void processFindFormEmptyResult() throws Exception {
         //actually this method here is not required, because by default 'clinicService' will return empty collection
         BDDMockito.when(clinicService.findOwnerByLastName(anyString())).thenReturn(Collections.emptyList());
 
@@ -66,6 +78,46 @@ class OwnerControllerTest {
             .andExpect(MockMvcResultMatchers.status().isOk())
             .andExpect(MockMvcResultMatchers.view().name("owners/findOwners"));
 //        BDDMockito.then(bindingResult).should().rejectValue(anyString(), anyString(), anyString());
+    }
+
+    @Test
+    void processFindFormOneResult() throws Exception {
+        Owner owner = new Owner();
+        owner.setId(8);
+        owner.setLastName("Smith");
+
+        BDDMockito.when(clinicService.findOwnerByLastName(owner.getLastName())).thenReturn(Lists.list(owner));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners")
+            .param("lastName", owner.getLastName()))
+            .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+            .andExpect(MockMvcResultMatchers.view().name("redirect:/owners/8"));
+
+        BDDMockito.then(clinicService).should().findOwnerByLastName(owner.getLastName());
+    }
+
+    @Test
+    void processFindFormManyResult() throws Exception {
+        Owner owner1 = new Owner();
+        Owner owner2 = new Owner();
+        String lastName = "last";
+
+        BDDMockito.when(clinicService.findOwnerByLastName(lastName)).thenReturn(Arrays.asList(owner1, owner2));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners")
+            .param("lastName", lastName))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.view().name("owners/ownersList"));
+
+        BDDMockito.then(clinicService).should().findOwnerByLastName(lastName);
+    }
+
+    @Test
+    void initCreationFormTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners/new"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.view().name("owners/createOrUpdateOwnerForm"))
+            .andExpect(MockMvcResultMatchers.model().attribute("owner", Matchers.any(Owner.class)));
     }
 
 }
